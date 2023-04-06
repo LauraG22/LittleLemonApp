897425e463ca9eb194e6faedcef9cc616e305089
Laura Gillis <laura.gillis2@gmail.com>
2023-04-06 11:51:58 -0400

    adding html structure

---
+++ src/Footer.js
@@ -0,0 +1,38 @@
+export default function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Menu</a>
+        </li>
+        <li>
+          <a href="#">Reservations</a>
+        </li>
+        <li>
+          <a href="#">Order Online</a>
+        </li>
+        <li>
+          <a href="#">Log In</a>
+        </li>
+      </ul>
+      <div>
+        <h3>Contact Info</h3>
+        <p>Address</p>
+        <p>Phone Number</p>
+        <p>Email</p>
+      </div>
+      <div>
+        <h3>Social Media</h3>
+        <p>Facebook</p>
+        <p>Twitter</p>
+        <p>Instagram</p>
+      </div>
+    </footer>
+  );
+}
---
+++ src/Header.js
@@ -0,0 +1,8 @@
+import logo from "./Logo.svg";
+export default function Header(props) {
+  return (
+    <header>
+      <img src={logo}></img>
+    </header>
+  );
+}
